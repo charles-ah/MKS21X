83a4ea11ff63515b665d06d0922cb49f6285f864
Charles Zhang <charleszhang2000@gmail.com>
2016-01-06 14:57:13 -0500

    bubble sort

--- hwSorts/Sorts.java
+++ hwSorts/Sorts.java
@@ -56,8 +56,9 @@ public class Sorts
 	    }
     }
 
-    public static void bubble(int nums[])
+    public static void bubbleSort(int nums[])
     {
+	/*
 	for(int i = 0;i<nums.length-1;i++)
 	    {
 		if(nums[i]<nums[i+1])
@@ -67,10 +68,33 @@ public class Sorts
 			nums[i+1] = temp;
 		    }
 	    }
+	*/
+	for(int i = 0;i<nums.length-1;i++)                                                                                                                                                                            {
+	    for(int j = 0;j<nums.length-i-1;j++)
+		{
+		    if(nums[j]>nums[j+1])                                                                                                                                                                   
+			{
+			    int temp = nums[j];                                                                                                                                                                
+			    nums[j] = nums[j+1];                                                                                                                                                          
+			    nums[j+1] = temp;                                                                                                                                                           
+                    }                                                                                                                                                                                      
+            } 
+	}
+	/*
 	for(int i = 0;i<nums.length;i++)
 	    {
-		
+		for(int j = 0;j < i;j++)
+		    {
+			if (nums[nums.length-1-j] < nums[nums.length-j-2])
+			    {
+				int temp = nums[nums.length-j-1];
+				nums[nums.length-j-1]=nums[nums.length-j-2];
+				nums[nums.length-j-2]=temp;
+			    }
+		    
+		    }
 	    }
+	*/
     }
     
     public static int min(int nums[])
@@ -88,7 +112,8 @@ public class Sorts
 
     public static void main(String[] args)
     {
-	int[] test1 = new int[] {87,65,51,80,6,74,2,64,15,78,20,37,8,61,95,45,92,9,17,15,61,93,68,81,76,86,34,84,93,16,77,27,59,77,83,34,77,1,65,11,1,78,44,94,24,6,91,71,14,74,26,44,52,12,26,75,11,72,75,49,89,4,24,83,71,20,13,63,46,73,97,85,67,69,0,83,53,50,77,44,86,40,51,49,53,99,16,19,6,87,44,32,95,45,78,41,39,97,5,35}	/*
+	int[] test1 = new int[] {87,65,51,80,6,74,2,64,15,78,20,37,8,61,95,45,92,9,17,15,61,93,68,81,76,86,34,84,93,16,77,27,59,77,83,34,77,1,65,11,1,78,44,94,24,6,91,71,14,74,26,44,52,12,26,75,11,72,75,49,89,4,24,83,71,20,13,63,46,73,97,85,67,69,0,83,53,50,77,44,86,40,51,49,53,99,16,19,6,87,44,32,95,45,78,41,39,97,5,35};
+	/*
 	for(int i = 0;i <100;i++)
 	    {
 		test1[i] = (int)(100*Math.random());
@@ -96,7 +121,7 @@ public class Sorts
 	*/
 
 	printArray(test1);
-	insertion(test1);
+	bubbleSort(test1);
 	printArray(test1);
 	
 
